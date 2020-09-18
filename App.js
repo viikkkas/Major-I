79804a871a43ee80c492f59f12278389a44dd3df
viikkkas <vikas.gupti047@gmail.com>
2020-09-18 12:34:29 +0530

    flatlist

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailScreen from "./app/screens/ListingDetailScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App(props) {
-  return <ListingDetailScreen />;
+  return <MessagesScreen />;
 }
