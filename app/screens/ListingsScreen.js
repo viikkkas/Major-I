d75609a906f5dcc5948861b9e47eab1374f5ed7f
viikkkas <vikas.gupti047@gmail.com>
2020-09-29 20:36:55 +0530

    Screen Update

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -3,6 +3,7 @@ import { FlatList, StyleSheet } from "react-native";
 
 import Screen from "../components/Screen";
 import Card from "../components/Card";
+import colors from "../config/colors";
 
 const listings = [
   {
@@ -21,7 +22,7 @@ const listings = [
 
 function ListingsScreen(props) {
   return (
-    <Screen>
+    <Screen style={styles.screen}>
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -36,5 +37,12 @@ function ListingsScreen(props) {
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
 
 export default ListingsScreen;
