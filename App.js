745f8de0be04a9a2ff9f93291dfe7dba4c861a70
viikkkas <vikas.gupti047@gmail.com>
2020-10-11 13:10:22 +0530

    Handling Picker Selection

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-import AppTextInput from "./app/components/AppTextInput"
+import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
 import Screen from "./app/components/Screen";
 
 const categories = [
-  { label: "Furniture", value: 1},
-  { label: "Clothing", value: 2},
-  { label: "Cameras", value: 3},
-]
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App(props) {
+  const [category, setCategory] = useState();
+
   return (
     <Screen>
-      <AppPicker items = {categories} placeholder="Category" icon="apps" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        placeholder="Category"
+        icon="apps"
+      />
       <AppTextInput placeholder="Email" icon="email" />
     </Screen>
   );
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -16,7 +16,7 @@ import AppText from "./AppText";
 import { FlatList } from "react-native-gesture-handler";
 import PickerItem from "./PickerItem";
 
-function AppPicker({ icon, items, placeholder }) {
+function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
@@ -31,7 +31,9 @@ function AppPicker({ icon, items, placeholder }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>{placeholder}</AppText>
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -48,7 +50,10 @@ function AppPicker({ icon, items, placeholder }) {
             renderItem={({ item }) => (
               <PickerItem
                 label={item.label}
-                onPress={() => console.log("Yes")}
+                onPress={() => {
+                  setModalVisible(false);
+                  onSelectItem(item);
+                }}
               />
             )}
           />
