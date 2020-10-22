fb4c5851505f8d0cbad1fe73427eec34d396a4ab
viikkkas <vikas.gupti047@gmail.com>
2020-10-22 12:33:27 +0530

    touched

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -29,7 +29,13 @@ function LoginScreen(props) {
           onSubmit={(values) => console.log(values)}
           validationSchema={validationSchema}
         >
-          {({ handleChange, handleSubmit, errors }) => (
+          {({
+            handleChange,
+            handleSubmit,
+            errors,
+            setFieldTouched,
+            touched,
+          }) => (
             <>
               <AppTextInput
                 autoCapitalize="none"
@@ -37,18 +43,23 @@ function LoginScreen(props) {
                 icon="email"
                 placeholder="Email"
                 keyboardType="email-address"
+                onBlur={() => setFieldTouched("email")}
                 onChangeText={handleChange("email")}
               />
-              <ErrorMessage error={errors.email} />
+              <ErrorMessage error={errors.email} visible={touched.email} />
               <AppTextInput
                 autoCapitalize="none"
                 autoCorrect={false}
                 icon="lock"
                 placeholder="Password"
+                onBlur={() => setFieldTouched("password")}
                 secureTextEntry={true}
                 onChangeText={handleChange("password")}
               />
-              <ErrorMessage error={errors.password} />
+              <ErrorMessage
+                error={errors.password}
+                visible={touched.password}
+              />
               <AppButton title="Submit" onPress={handleSubmit} />
             </>
           )}
